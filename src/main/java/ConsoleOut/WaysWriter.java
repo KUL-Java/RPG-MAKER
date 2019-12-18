64ef7e3b14200a0040c25e812a8f0d89ffc3d938
PawelPaziewski <lrpapawel@gmail.com>
2019-12-18 12:03:16 +0100

    10-10-Make a console out

--- src/main/java/ConsoleOut/WaysWriter.java
+++ src/main/java/ConsoleOut/WaysWriter.java
@@ -2,18 +2,20 @@ package ConsoleOut;
 
 import Main.Way;
 import java.util.Map;
-
+import java.util.Map.Entry;
+import java.util.Set;
 public class WaysWriter {
     public void Write(Map<Character, Way> ways, int delay, String color){
-        for (Character name: ways.keySet()){
+        Set<Entry<Character, Way>> entrySet=ways.entrySet();
+        for(Entry<Character, Way> entry:entrySet)
+        {
             try {
                 Thread.sleep(10*delay);
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            String letter = name.toString();
-            Way way = ways.get(name);
-            System.out.println(color+"["+letter + "] " + way);
+            System.out.println(color+"["+entry.getKey()+"] "+entry.getValue());
+
         }
     }
 }
