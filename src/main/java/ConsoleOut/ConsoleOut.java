f30d178092ce531058413a6ac41a49ad7c92fc0d
PawelPaziewski <lrpapawel@gmail.com>
2019-12-22 22:57:52 +0100

    10-10-Make a console out

--- src/main/java/ConsoleOut/ConsoleOut.java
+++ src/main/java/ConsoleOut/ConsoleOut.java
@@ -1,19 +1,46 @@
 package ConsoleOut;
+
 import Main.History;
+import Main.Way;
+
+import java.util.Map;
+import java.util.Set;
 
 public class ConsoleOut implements UserWriter {
-    private TextWriter textWriter;
-    private WaysWriter waysWriter;
     private int delay;
     private String color;
+
     public ConsoleOut(int delay, String color) {
-        this.textWriter = new TextWriter();
-        this.waysWriter = new WaysWriter();
-        this.color= ColorCoder.getColorCode(color);
+        this.color = ColorCoder.getColorCode(color);
         this.delay = delay;
     }
-    public void writeHistory(History history){
-        textWriter.Write(history.getHistory(), delay, color);
-        waysWriter.Write(history.getWays(), delay, color);
+
+    public void writeHistory(History history) {
+        writeStory(history.getHistory());
+        writeWays(history.getWays());
+    }
+
+    void writeStory(String text) {
+        for (char c : text.toCharArray()) {
+            System.out.print(color + c);
+            try {
+                Thread.sleep(delay);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println();
+    }
+
+    public void writeWays(Map<Character, Way> ways) {
+        Set<Map.Entry<Character, Way>> entrySet = ways.entrySet();
+        for (Map.Entry<Character, Way> entry : entrySet) {
+            try {
+                Thread.sleep(10 * delay);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println(color + "[" + entry.getKey() + "] " + entry.getValue());
+        }
     }
 }
